2132e3f4ace1a7acce6d4175570586b0b64315b0
Nicolas Elias <nicolaselias@pm.me>
2024-09-27 09:38:51 +0000

    fear: criando springboot e estruturando diretorios

---
+++ backend/src/test/java/br.ufsc.sigcaps/SigcapsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.ufsc.sigcaps.sigcaps;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SigcapsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
