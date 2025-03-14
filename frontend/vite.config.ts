857278b887d38b1fb7e994b7a00de7319f5a3f36
Nicolas Elias <nicolaselias@pm.me>
2025-03-14 15:19:46 -0300

    fix: global irrelevante

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,9 +3,6 @@ import react from "@vitejs/plugin-react-swc";
 
 // https://vite.dev/config/
 export default defineConfig({
-  define: {
-    global: "window",
-  },
   plugins: [react()],
   build: {
     outDir: "build",
